e763dc2fe09a0ccf938048a3cbd1f5746ddba0fa
Kartik B <kartikbhalla12@gmail.com>
2020-07-04 02:58:23 +0530

    redux: section 2 code updates
    Signed-off-by: Kartik B <kartikbhalla12@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+import { pipe } from 'lodash/fp';
+import { produce } from 'immer';
+
+const input = '    JavaScript    ';
+
+const trim = str => str.trim();
+const toLowerCase = str => str.toLowerCase();
+const wrap = type => str => `<${type}>${str}</${type}>`;
+
+const transform = pipe(trim, toLowerCase, wrap('span'));
+const result = transform(input);
+console.log(result);
+
+let book = { title: 'Harry Potter' };
+
+function publishBook(book) {
+	return produce(book, draftBook => {
+		draftBook.isPublished = true;
+	});
+}
+const updated = publishBook(book);
+
+console.log(book, updated);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 9000
-  },
-  mode: "development"
+	entry: './src/index.js',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 8080,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+			'Access-Control-Allow-Headers':
+				'X-Requested-With, content-type, Authorization',
+		},
+	},
+	mode: 'development',
 };
